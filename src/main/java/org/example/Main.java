636eef0d5f96ebd7fad1d70bc653cf8bd27bafd6
EllenRoda <programacr001@gmail.com>
2025-02-18 16:46:59 -0300

    primeiro commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("");
         System.out.println("------------------");
-        System.out.println("| Projeto_no_Git |");
+        System.out.println("* Projeto_no_Git *");
         System.out.println("------------------");
     }
 }
